1f2602ed9560b9701160676263f9e0f9868109dc
Taylor C. MacDonald <tcmacdonald@gmail.com>
2011-06-16 13:43:40 -0400

    initial commit:

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MsaarchCom::Application.config.session_store :cookie_store, key: '_msaarch-com_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MsaarchCom::Application.config.session_store :active_record_store
