d6a7072c39a3a3becc055ff584e7c23c4ec1100a
Taylor C. MacDonald <tcmacdonald@gmail.com>
2011-08-05 13:47:38 -0400

    redirect users on /portfolio who haven't specified a market

--- app/controllers/public/pages_controller.rb
+++ app/controllers/public/pages_controller.rb
@@ -2,13 +2,17 @@ class Public::PagesController < PublicController
 
   def show
     raise ActiveRecord::RecordNotFound if current_page.nil?
-    respond_to do |format|
-      format.html do
-        #@page_title = META[:page_title]
-        @page_title = "#{current_page.nav_name} | #{@page_title}"
-        @meta_keywords = current_page.meta_keywords
-        @meta_description = current_page.meta_description
-        render :template => current_page.template.view_path
+    if params[:permalink] == 'portfolio'
+      redirect_to root_url 
+    else
+      respond_to do |format|
+        format.html do
+          @page_title = META[:page_title]
+          @page_title = "#{current_page.nav_name} | #{@page_title}"
+          @meta_keywords = current_page.meta_keywords
+          @meta_description = current_page.meta_description
+          render :template => current_page.template.view_path
+        end
       end
     end
   end
