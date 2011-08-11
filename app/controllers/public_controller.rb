cd42843a4bc22e0351a9752d43c3e8d3cdb63806
Taylor C. MacDonald <tcmacdonald@gmail.com>
2011-08-11 16:38:49 -0400

    add 404 and 500 error pages

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,10 +1,15 @@
 class PublicController < ApplicationController
 
+  unless Rails.env.development?
+   rescue_from Exception, :with => :error_message
+   rescue_from ActiveRecord::RecordNotFound, :with => :file_not_found
+  end
+
   def home
     params[:permalink] = 'index'
   end
 
-  private
+  protected
 
     helper_method :current_market, :current_features
 
@@ -14,6 +19,17 @@ class PublicController < ApplicationController
 
     def current_features
       @current_features ||= current_page.features.live.collect{ |feature| feature if ['feature','video'].include?(feature.feature_type) }[0..4]
+    end
+
+    def error_message(e)
+      HoptoadNotifier.notify(e)
+      params[:permalink] = 'index'
+      render :template => 'public/errors/500', :status => 500
+    end
+
+    def file_not_found
+      params[:permalink] = 'index'
+      render :template => 'public/errors/404', :status => 404
     end
 
 end
