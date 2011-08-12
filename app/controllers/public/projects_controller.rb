b170f54769587920775699285b09e497afc4e387
Taylor C. MacDonald <tcmacdonald@gmail.com>
2011-08-12 12:34:56 -0400

    hook up meta content on front-end

--- app/controllers/public/projects_controller.rb
+++ app/controllers/public/projects_controller.rb
@@ -2,6 +2,10 @@ class Public::ProjectsController < PublicController
 
   def show
     raise ActiveRecord::RecordNotFound if current_project.nil?
+    @page_title = META[:page_title]
+    @page_title = "#{current_project.nav_name} | #{@page_title}"
+    @meta_keywords = current_project.meta_keywords
+    @meta_description = current_project.meta_description
   end
 
   private
