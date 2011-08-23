33f5948ffeea904b9398bafa1a4efe23aeb659f1
Taylor C. MacDonald <tcmacdonald@gmail.com>
2011-08-23 11:33:28 -0400

    ensure admin's can zero out multi select fields

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -5,6 +5,9 @@ class Admin::ProjectsController < AdminController
   
   def update 
     super
+    empty_similar unless params[:project].has_key?(:similar_project_ids)
+    empty_markets unless params[:project].has_key?(:market_ids)
+    empty_users unless params[:project].has_key?(:user_ids)
     Market.update_project_counters
   end
   
@@ -12,5 +15,19 @@ class Admin::ProjectsController < AdminController
     super
     Market.update_project_counters
   end
+
+  private
+  
+    def empty_markets
+      current_object.update_attribute :market_ids, nil 
+    end
+
+    def empty_users
+      current_object.update_attribute :user_ids, nil 
+    end
+    
+    def empty_similar
+      current_object.update_attribute :similar_project_ids, nil 
+    end
 
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,13 @@
 class Admin::UsersController < AdminController
 
+  def update
+    params[:user].reject! { |k,v| k.include?('password') && v.blank? }
+    super
+    empty_projects unless params[:user].has_key?(:project_ids)
+    empty_favorites unless params[:user].has_key?(:favorite_ids)
+    empty_expertise unless params[:user].has_key?(:expertise_ids)
+  end
+
   protected 
 
     helper_method :current_admin_users, :current_staff_users
@@ -12,6 +20,20 @@ class Admin::UsersController < AdminController
     def current_staff_users
       per_page = params[:per_page] || 15
       User.all.paginate(:page => params[:staff_users_page], :per_page => per_page)
+    end
+
+  private 
+
+    def empty_projects
+      current_object.update_attribute :project_ids, nil 
+    end
+
+    def empty_favorites
+      current_object.update_attribute :favorite_ids, nil 
+    end
+
+    def empty_expertise
+      current_object.update_attribute :expertise_ids, nil 
     end
 
 end
