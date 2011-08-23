b052286fb69f1db4c469dfc5b949b38bf893fc17
Taylor C. MacDonald <tcmacdonald@gmail.com>
2011-08-23 15:25:02 -0400

    bug squashing

--- app/controllers/admin/markets_controller.rb
+++ app/controllers/admin/markets_controller.rb
@@ -3,9 +3,10 @@ class Admin::MarketsController < AdminController
   set_model :market
   
   def update_featured
+    #raise current_market.to_yaml
     projectship_ids = params[:featured_projectship_ids].try(:keys) || []
-    Projectship.update_all({:featured => false})
-    Projectship.update_all({:featured => true}, {:id => projectship_ids})
+    Projectship.update_all({:featured => false}, {:owner_id => current_market.id, :owner_type => 'Market' })
+    Projectship.update_all({:featured => true}, {:id => projectship_ids, :owner_id => current_market.id, :owner_type => 'Market' })
     render :nothing => true
   end
   
