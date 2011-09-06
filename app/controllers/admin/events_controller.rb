80f38386cf627ac6884a40c46c19f2dc4c694914
Taylor C. MacDonald <tcmacdonald@gmail.com>
2011-09-06 09:42:01 -0400

    ensure all awards are accessible within the admin tool

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -7,7 +7,7 @@ class Admin::EventsController < AdminController
     helper_method :current_events
   
     def current_events
-      Event.live.group_by(&:event_date).sort_by{ |a, b| a.strftime('%Y') }.reverse
+      Event.all.group_by(&:event_date).sort_by{ |a, b| a.strftime('%Y') }.reverse
     end
   
 end
