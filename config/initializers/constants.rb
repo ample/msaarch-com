61ae657bd6ddd6259a59a10b86e98d3dde6f5a46
Taylor C. MacDonald <tcmacdonald@gmail.com>
2011-09-13 10:18:32 -0400

    default meta content

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,6 @@
 META = {
-  :keywords => '@see config/initializers/constants.rb',
-  :description => '@see config/initializers/constants.rb',
+  :keywords => 'Architects, architecture, designs, Michael Schuster, Associates, Cincinnati, Columbus, Ohio, Master planning, Wayfinding',
+  :description => 'MSA architects designs for 7 core markets including education, corporate, lifestyle, sport, government, religious and branded engagements',
   :page_title => 'MSA Architects'
 }
 
