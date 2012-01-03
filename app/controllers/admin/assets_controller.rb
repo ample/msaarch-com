20e80269b4b99a200d130b717b660e93eccda0b3
Taylor C. MacDonald <tcmacdonald@gmail.com>
2012-01-03 13:46:51 -0500

    assets aren't ordered properly because its bubbling up to admin#index instead of admin/assets#index. this solution is by no means elegant but rather a quick-fix

--- app/controllers/admin/assets_controller.rb
+++ app/controllers/admin/assets_controller.rb
@@ -1,9 +1,34 @@
 class Admin::AssetsController < AdminController
   
   def index
-    params[:per_page] = 100 unless params[:per_page]
-    super
+    per_page = params[:per_page] || 100
+    conditions = nil
+
+    if params[:type] == 'images'
+      conditions = ["attachment_mime_type in (?)", ['image/jpeg','image/png','image/gif']]
+    elsif params[:type] == 'documents'
+      conditions = ["attachment_mime_type NOT in (?)", ['image/jpeg','image/png','image/gif']]
+    end
+
+    if params[:order] == 'touched_at'
+      order = 'updated_at DESC'
+    else
+      order = 'created_at DESC'
+    end
+    
+    if per_page.nil?
+      @current_objects = Asset.find(:all, :order => order, :conditions => conditions)
+    else 
+      @current_objects = Asset.find(:all, :order => order, :conditions => conditions).paginate(:page => params[:page], :per_page => per_page)
+    end    
+
+    respond_to do |format|
+      format.js { render :partial => 'admin/assets/asset_page', :content_type => :html }
+      format.json { @current_objects }
+      format.html { @current_objects }
+    end
   end
+
   
   def show
     raise ActiveRecord::RecordNotFound if current_asset.nil?
