b5ec63bbe1ce35ef1a0499fde786e5f1d9736ba5
Taylor C. MacDonald <tcmacdonald@gmail.com>
2012-01-11 15:51:29 -0500

    add page support to careers controller show action.

--- app/controllers/public/careers_controller.rb
+++ app/controllers/public/careers_controller.rb
@@ -8,7 +8,18 @@ class Public::CareersController < PublicController
   end
 
   def show
-    raise ActiveRecord::RecordNotFound if current_career.nil?
+    raise ActiveRecord::RecordNotFound if current_career.nil? && current_page.nil?
+    if current_page
+      respond_to do |format|
+        format.html do
+          @page_title = META[:page_title]
+          @page_title = "#{current_page.nav_name} | #{@page_title}"
+          @meta_keywords = current_page.meta_keywords
+          @meta_description = current_page.meta_description
+          render :template => current_page.template.view_path
+        end
+      end
+    end
   end
 
   def new
