2d4251db4b73bee80085e70d10703eedf30c4890
Taylor C. MacDonald <tcmacdonald@gmail.com>
2012-06-11 19:28:20 -0400

    Loose extraneous column.

--- db/migrate/20120611205243_add_twitter_id_to_updates.rb
+++ db/migrate/20120611205243_add_twitter_id_to_updates.rb
@@ -1,7 +1,6 @@
 class AddTwitterIdToUpdates < ActiveRecord::Migration
   def change
     add_column :updates, :twitter_id, :string
-    add_column :updates, :publish_to_twitter, :boolean
     add_index :updates, :twitter_id, :unique => true
   end
 end
