1cf0a1ae6989cb431ed9ca64bda61ef00338114e
Taylor C. MacDonald <tcmacdonald@gmail.com>
2012-06-18 10:16:49 -0400

    New oauth credentials for read/write app.

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 Twitter.configure do |config|
   config.consumer_key = 'gb3VTWv5JTI6MOB3KgpLlQ'
   config.consumer_secret = 'o53PmvgSGn9aStx2bmCMlSBIFxycYHfavbVY7RoUQyU'
-  config.oauth_token = '485094683-1OE9zI0MjuysP868dgDdKioWQn8tn5ngRgXgNh1Y'
-  config.oauth_token_secret = 'X5lCEZnCOewkmGFRZGrMjEFPbbiTrXYIhMyI5RBNOxI'
+  config.oauth_token = '485094683-vtSnGjjOSjVoVCajqxqp2dH8VkTbs7C9FP1OpD0g'
+  config.oauth_token_secret = 'UdHb9ew73Nc7WmyV7SzI9diCMMJvrK3BUGLyQ9KryA'
 end
