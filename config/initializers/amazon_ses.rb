dabeadbff1d63e02021103227b0361d6a4d4ca40
Taylor C. MacDonald <tcmacdonald@gmail.com>
2014-06-03 09:02:53 -0400

    Update SES credentials.

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,3 +1,3 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  :access_key_id     => '05E9M3Q690VXTHYXSX82',
-  :secret_access_key => 'hKdDtMMW/BH2e0erWQ1eHfMm0rhimvEdizX5bTUX'
+  :access_key_id     => 'AKIAIHNAQYK6FV7YLP5Q',
+  :secret_access_key => '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'
