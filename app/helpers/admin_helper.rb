e1d572153c311500552d415f0d56cb3f9e46daf4
Taylor C. MacDonald <tcmacdonald@gmail.com>
2015-12-14 14:58:02 -0500

    Leverage body text instead of title for project captions.

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -5,4 +5,15 @@ module AdminHelper
     content_tag :li, ("#{object.class.name}: " + link_to(eval("object.#{title}"), eval("edit_#{object.class.name.tableize.singularize}_path(object)"))).html_safe if object
   end
 
+  def nesting_object
+    @nesting_object ||= begin
+      params.each do |name, value|
+        if name =~ /^([^_]+)_id$/
+          klass = "#{$1}".classify.constantize
+          return klass.find(value)
+        end
+      end
+      nil
+    end
+  end
 end
