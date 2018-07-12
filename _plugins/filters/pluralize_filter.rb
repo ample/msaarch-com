d039db3028b1c9e79e62c532ff35bce0dac75a1e
Taylor MacDonald <tcmacdonald@gmail.com>
2018-07-11 23:38:15 -0400

    Add pluralize filter

---
+++ _plugins/filters/pluralize_filter.rb
@@ -0,0 +1,11 @@
+module Jekyll
+  module Pluralize
+    include ActiveSupport::Inflector
+
+    def pluralize(str)
+      str.pluralize
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::Pluralize)
