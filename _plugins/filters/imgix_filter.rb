8aa1bbcfa7562dff34f347abc53e8ddfd1962a81
Taylor MacDonald <tcmacdonald@gmail.com>
2026-02-02 12:13:48 -0500

    🩹 fix: preserve aspect ratio in imgix_srcset for responsive images
    Calculate proportional height for each srcset width based on the original
    w/h ratio, ensuring images maintain consistent aspect ratio across all
    responsive breakpoints.

--- _plugins/filters/imgix_filter.rb
+++ _plugins/filters/imgix_filter.rb
@@ -11,19 +11,39 @@ module Jekyll
     def imgix_srcset(url, ix_params=nil)
       widths = [320, 640, 960, 1280, 1920, 2560]
       
-      # Parse ix_params to extract 'h' and other parameters, ignoring 'w'
-      parsed_params = {}
+      original_w = nil
+      original_h = nil
+      other_params = []
+
       unless ix_params.nil?
         ix_params.split('&').each do |param|
           key, value = param.split('=')
-          parsed_params[key] = value unless key == 'w' # Ignore 'w' from original params
+          if key == 'w'
+            original_w = value.to_i
+          elsif key == 'h'
+            original_h = value.to_i
+          else
+            other_params << param
+          end
         end
       end
 
+      aspect_ratio = nil
+      if original_w && original_h && original_h > 0
+        aspect_ratio = original_w.to_f / original_h.to_f
+      end
+
       srcset = widths.map do |w|
-        current_params = parsed_params.merge('w' => w)
+        current_params_list = ["w=#{w}"]
+        if aspect_ratio
+          h = (w / aspect_ratio).round
+          current_params_list << "h=#{h}"
+        elsif original_h # If only h was provided, keep it fixed
+          current_params_list << "h=#{original_h}"
+        end
         
-        param_string = current_params.map { |k, v| "#{k}=#{v}" }.join('&')
+        current_params_list += other_params
+        param_string = current_params_list.join('&')
         
         imgix_url = imgix(url, param_string)
         "#{imgix_url} #{w}w"
