19289a3210f3e013e77d5571d60e5e68b48751fa
Taylor MacDonald <tcmacdonald@gmail.com>
2026-02-02 11:27:08 -0500

    🩹 fix: pass explicit dimensions to imgix_srcset for consistent image sizing
    Update imgix_srcset filter to properly parse width/height params and
    apply consistent dimensions across responsive breakpoints. Standardize
    card images to 275x210 and media objects to 350x350.

--- _plugins/filters/imgix_filter.rb
+++ _plugins/filters/imgix_filter.rb
@@ -10,8 +10,22 @@ module Jekyll
 
     def imgix_srcset(url, ix_params=nil)
       widths = [320, 640, 960, 1280, 1920, 2560]
+      
+      # Parse ix_params to extract 'h' and other parameters, ignoring 'w'
+      parsed_params = {}
+      unless ix_params.nil?
+        ix_params.split('&').each do |param|
+          key, value = param.split('=')
+          parsed_params[key] = value unless key == 'w' # Ignore 'w' from original params
+        end
+      end
+
       srcset = widths.map do |w|
-        imgix_url = imgix(url, "w=#{w}#{"&#{ix_params}" unless ix_params.nil?}")
+        current_params = parsed_params.merge('w' => w)
+        
+        param_string = current_params.map { |k, v| "#{k}=#{v}" }.join('&')
+        
+        imgix_url = imgix(url, param_string)
         "#{imgix_url} #{w}w"
       end.join(', ')
       srcset
